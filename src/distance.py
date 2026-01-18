96baba751a5714635e0d25b9410b52a4329ff6b8
luisparadinha <luis.m.paradinha@gmail.com>
2026-01-18 10:51:24 +0000

    defini a função de calculo de  distâncias

---
+++ src/distance.py
@@ -0,0 +1,28 @@
+def distance(v1, v2, metric = 'euclidian'):
+    """
+    Computa a distância entre dois pontos num espaço N-dimensional.
+    Os inputs têm de ser vetores com a mesma dimensão.
+
+    Parâmetros:
+    v1 : array numpy com coordenadas do primeiro ponto
+    v2 : array numpy com coordenadas do segundo ponto
+    metric : euclidiana, manhattan
+
+    Devolve:
+    dist: distância entre v1 e v2 (float)
+    """        
+    if v1.shape != v2.shape: # verifica se v1 e v2 têm a mesma dimensão
+        raise ValueError("v1 and v2 têm de ter a mesma dimensão") # devolve erro
+    else:
+        if metric == 'euclidian':
+            dist_euc = np.sqrt(np.sum((v1 - v2) ** 2)) # computa distância euclidiana, numpy faz o somatório da substração elemento a elemento e o quadrado também
+            return dist_euc # devolve a distância euclidiana
+        elif metric == 'manhattan':
+            dist_manhattan = np.sum(np.abs(v1 - v2))
+            return dist_manhattan
+        else: 
+            raise ValueError(f"Unknown metric: {metric}. Please choose one of the available metrics")
+    
+
+    
+    
